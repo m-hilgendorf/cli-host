7080807038d400e398bf0f5dac7306237e7961ee
m-hilgendorf <mike@hilgendorf.audio>
2019-06-20 21:08:44 -0700

    formatting

--- src/host_impl.rs
+++ src/host_impl.rs
@@ -1,37 +1,45 @@
 use crate::*;
 use std::ptr::null_mut;
-use widestring::{WideCStr, U16Str};
-use vst3_impl::*;
 use vst3_derive::*;
+use vst3_impl::*;
+use widestring::{U16Str, WideCStr};
 
-pub enum ProcessMode { RealTime, PreFetch, Offline }
-pub enum SampleSize { F32, F64 }
-use widestring::U16CStr;
+pub enum ProcessMode {
+    RealTime,
+    PreFetch,
+    Offline,
+}
+pub enum SampleSize {
+    F32,
+    F64,
+}
 use std::mem::transmute;
 use std::os::raw::c_void;
+use widestring::U16CStr;
 
 /// Safe wrapper around IAudioProcessor
 pub struct Processor(VstPtr<IAudioProcessor>);
 impl Processor {
     //todo: take a host as an argument for the initialization method
-    pub fn new (processor : VstPtr<IAudioProcessor>) -> Self {
+    pub fn new(processor: VstPtr<IAudioProcessor>) -> Self {
         unsafe {
-            (*processor.cast::<IPluginBase>()
+            (*processor
+                .cast::<IPluginBase>()
                 .expect("Processor doesn't implement IPluginBase"))
-                .initialize(null_mut());
+            .initialize(null_mut());
         }
-        Self (processor)
+        Self(processor)
     }
 
-    fn setup_busses(&self, flags : SpeakerArrangement) -> Result<(), tresult>  {
+    fn setup_busses(&self, flags: SpeakerArrangement) -> Result<(), tresult> {
         let mut arr = flags;
-        let e = unsafe {
-            (*self.0).setBusArrangements(
-                &mut arr as *mut _, 1,
-                &mut arr as *mut _, 1)
-        };
-        if e != 0 { Err(e) }
-        else { Ok(()) }
+        let e =
+            unsafe { (*self.0).setBusArrangements(&mut arr as *mut _, 1, &mut arr as *mut _, 1) };
+        if e != 0 {
+            Err(e)
+        } else {
+            Ok(())
+        }
     }
 
     //todo: support exotic speaker arrangments
@@ -49,32 +57,37 @@ impl Processor {
     }
 
     //todo: give this a generic argument?
-    pub fn setup_processing(&self,
-                            mode : ProcessMode,
-                            sample_size : SampleSize,
-                            max_block_size : usize,
-                            sample_rate : f64) -> Result<(), tresult> {
+    pub fn setup_processing(
+        &self,
+        mode: ProcessMode,
+        sample_size: SampleSize,
+        max_block_size: usize,
+        sample_rate: f64,
+    ) -> Result<(), tresult> {
         let pmode = match mode {
             ProcessMode::RealTime => ProcessModes::kRealtime,
             ProcessMode::PreFetch => ProcessModes::kPrefetch,
-            ProcessMode::Offline => ProcessModes::kOffline
+            ProcessMode::Offline => ProcessModes::kOffline,
         };
 
         let sampleSize = match sample_size {
             SampleSize::F32 => SymbolicSampleSizes::kSample32,
-            SampleSize::F64 => SymbolicSampleSizes::kSample64
+            SampleSize::F64 => SymbolicSampleSizes::kSample64,
         };
 
         let mut setup = ProcessSetup {
-            processMode : pmode,
-            symbolicSampleSize : sampleSize,
-            maxSamplesPerblock : max_block_size as i32,
-            sampleRate : sample_rate
+            processMode: pmode,
+            symbolicSampleSize: sampleSize,
+            maxSamplesPerblock: max_block_size as i32,
+            sampleRate: sample_rate,
         };
-        let e = unsafe { (*self.0).setupProcessing (&mut setup as *mut _) };
+        let e = unsafe { (*self.0).setupProcessing(&mut setup as *mut _) };
 
-        if e != 0 { Err(e) }
-        else {Ok(()) }
+        if e != 0 {
+            Err(e)
+        } else {
+            Ok(())
+        }
     }
 
     pub fn tail_samples(&self) -> usize {
@@ -89,9 +102,7 @@ impl Processor {
 impl Drop for Processor {
     fn drop(&mut self) {
         unsafe {
-            (*(self.0.cast::<IPluginBase>()
-                .unwrap()))
-                .terminate();
+            (*(self.0.cast::<IPluginBase>().unwrap())).terminate();
         }
     }
 }
@@ -101,75 +112,85 @@ pub struct Editor(VstPtr<IEditController>);
 
 #[allow(unused_variables)]
 impl Editor {
-    pub fn new(editor : VstPtr<IEditController>) -> Self {
+    pub fn new(editor: VstPtr<IEditController>) -> Self {
         unsafe {
-            (*editor.cast::<IPluginBase>()
+            (*editor
+                .cast::<IPluginBase>()
                 .expect("Processor doesn't implement IPluginBase"))
-                .initialize(null_mut());
+            .initialize(null_mut());
         }
-        Self (editor)
+        Self(editor)
     }
 
-    pub fn set_state(&self, stream : &BStream) -> Result<(), tresult> {
-        let e = unsafe {
-            (*self.0).setState(stream.0.as_raw())
-        };
+    pub fn set_state(&self, stream: &BStream) -> Result<(), tresult> {
+        let e = unsafe { (*self.0).setState(stream.0.as_raw()) };
 
-        if e != 0 { Err(e) }
-        else { Ok(()) }
+        if e != 0 {
+            Err(e)
+        } else {
+            Ok(())
+        }
     }
 
-    pub fn get_state(&self, stream : &BStream) -> Result<(), tresult> {
-        let e = unsafe {
-            (*self.0).getState(stream.0.as_raw())
-        };
+    pub fn get_state(&self, stream: &BStream) -> Result<(), tresult> {
+        let e = unsafe { (*self.0).getState(stream.0.as_raw()) };
 
-        if e != 0 { Err(e) }
-        else { Ok(()) }
+        if e != 0 {
+            Err(e)
+        } else {
+            Ok(())
+        }
     }
 
     pub fn get_parameter_count(&self) -> usize {
         unsafe { self.0.getParameterCount() as usize }
     }
 
-    pub fn get_parameter_info(&self, index : usize) -> Result<ParameterInfo, tresult> {
+    pub fn get_parameter_info(&self, index: usize) -> Result<ParameterInfo, tresult> {
         let mut info = ParameterInfo::default();
         let e = unsafe { self.0.getParameterInfo(index as i32, &mut info as *mut _) };
 
-        if e != 0 { Err(e) }
-        else { Ok(info) }
+        if e != 0 {
+            Err(e)
+        } else {
+            Ok(info)
+        }
     }
 
     //todo: make this not allocate
-    pub fn get_param_string_by_value(&self, id : ParamID, value : f64)-> Result<String, tresult> {
+    pub fn get_param_string_by_value(&self, id: ParamID, value: f64) -> Result<String, tresult> {
         let mut s = [0; 128];
-        let e = unsafe {
-            self.0.getParamStringByValue(id, value, s.as_mut_ptr())
-        };
-        if e != 0 { Err(e) }
-        else { unsafe {  Ok(U16CStr::from_ptr_str(transmute(s.as_ptr())).to_string_lossy() ) } }
+        let e = unsafe { self.0.getParamStringByValue(id, value, s.as_mut_ptr()) };
+        if e != 0 {
+            Err(e)
+        } else {
+            unsafe { Ok(U16CStr::from_ptr_str(transmute(s.as_ptr())).to_string_lossy()) }
+        }
     }
 
     // todo: utf8 version
-    pub fn get_param_value_by_string(id : ParamID, string : &U16CStr) -> Result<f64, tresult> {
+    pub fn get_param_value_by_string(id: ParamID, string: &U16CStr) -> Result<f64, tresult> {
         //todo:
         unimplemented!()
     }
-    pub fn normalized_param_to_plain (&self, id : ParamID, normalized : f64) -> f64 {
+    pub fn normalized_param_to_plain(&self, id: ParamID, normalized: f64) -> f64 {
         unsafe { self.0.normalizedParamToPlain(id, normalized) }
     }
-    pub fn plain_param_to_normlaized (&self, id : ParamID, normalized : f64) -> f64 {
+    pub fn plain_param_to_normlaized(&self, id: ParamID, normalized: f64) -> f64 {
         unsafe { self.0.plainParamToNormalized(id, normalized) }
     }
-    pub fn get_param_normalized (&self, id : ParamID) -> f64 {
+    pub fn get_param_normalized(&self, id: ParamID) -> f64 {
         unsafe { self.0.getParamNormalized(id) }
     }
-    pub fn set_param_normalized (&self, id : ParamID, value : f64) -> Result<(), tresult> {
+    pub fn set_param_normalized(&self, id: ParamID, value: f64) -> Result<(), tresult> {
         let e = unsafe { self.0.setParamNormalized(id, value) };
-        if e != 0 { Err(e) }
-        else { Ok(()) }
+        if e != 0 {
+            Err(e)
+        } else {
+            Ok(())
+        }
     }
-    pub fn set_component_handler(&self, handler : ComponentHandler) -> Result<(), tresult> {
+    pub fn set_component_handler(&self, handler: ComponentHandler) -> Result<(), tresult> {
         Err(-1)
     }
     pub fn create_view(&self) -> PlugView {
@@ -184,39 +205,48 @@ impl Editor {
 pub struct ComponentHandler(VstPtr<IComponentHandler>);
 pub struct PlugView(VstPtr<IPlugView>);
 impl PlugView {
-    pub fn new(view : VstPtr<IPlugView>) -> Self {
+    pub fn new(view: VstPtr<IPlugView>) -> Self {
         Self(view)
     }
 
-    pub fn attached(&self, parent : *mut c_void) -> Result<(), tresult>{ unsafe {
-        let platform: *mut i8 = transmute ("X11EmbedWindowID\0".as_ptr());
-        let e = self.0.attached(parent, platform);
-        if e != 0 { Err(e) }
-        else { Ok(())}
-    }}
+    pub fn attached(&self, parent: *mut c_void) -> Result<(), tresult> {
+        unsafe {
+            let platform: *mut i8 = transmute("X11EmbedWindowID\0".as_ptr());
+            let e = self.0.attached(parent, platform);
+            if e != 0 {
+                Err(e)
+            } else {
+                Ok(())
+            }
+        }
+    }
 
-    pub fn get_size(&self) -> Result<ViewRect,tresult> {
+    pub fn get_size(&self) -> Result<ViewRect, tresult> {
         let mut rect = ViewRect {
-            left : 0,
-            right : 0,
-            top : 0,
-            bottom : 0
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
         };
-        let e = unsafe {
-            self.0.getSize(&mut rect as *mut _)
-        };
-        if e != 0 { Err(e) }
-        else { Ok(rect) }
+        let e = unsafe { self.0.getSize(&mut rect as *mut _) };
+        if e != 0 {
+            Err(e)
+        } else {
+            Ok(rect)
+        }
     }
 
-    pub fn removed(&self)-> Result<(), tresult> {
+    pub fn removed(&self) -> Result<(), tresult> {
         let e = unsafe { self.0.removed() };
-        if e != 0 { Err(e) }
-        else { Ok(())}
+        if e != 0 {
+            Err(e)
+        } else {
+            Ok(())
+        }
     }
 
-    pub fn set_frame(&self, frame : *mut IPlugFrame) {
-    //    unsafe { self.0.setFrame(frame.as_raw() as *mut _) };
+    pub fn set_frame(&self, frame: *mut IPlugFrame) {
+        //    unsafe { self.0.setFrame(frame.as_raw() as *mut _) };
         unsafe { self.0.setFrame(frame as *mut _) };
     }
 }
@@ -225,8 +255,8 @@ impl PlugView {
 #[derive(Vst3Impl)]
 #[interfaces(IPlugFrame)]
 pub struct HostPlugFrame {
-    vtbl : VTable<IPlugFrameVtbl>,
-    refcount : Refcount
+    vtbl: VTable<IPlugFrameVtbl>,
+    refcount: Refcount,
 }
 
 impl HostPlugFrame {
@@ -239,10 +269,13 @@ impl HostPlugFrame {
 
 #[vst3_impl]
 unsafe impl IPlugFrame for HostPlugFrame {
-    fn resizeView(&self, view : *mut IPlugView, newSize : *mut ViewRect) -> tresult{
+    fn resizeView(&self, view: *mut IPlugView, newSize: *mut ViewRect) -> tresult {
         println!("view: {:?}", view);
-        let size  = unsafe { &*newSize };
-        println!("size: {},{},{},{}", size.top, size.bottom, size.left, size.right);
+        let size = unsafe { &*newSize };
+        println!(
+            "size: {},{},{},{}",
+            size.top, size.bottom, size.left, size.right
+        );
         0
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,19 @@ mod pluginterfaces;
 extern crate structopt;
 extern crate libloading as ll;
 
-use winit::{Event, WindowEvent, WindowBuilder, EventsLoop};
-use winit::dpi::LogicalSize;
+use std::mem::forget;
 use std::mem::transmute;
-use x11::xlib::XInternAtom;
-use structopt::StructOpt;
-use std::path::PathBuf; 
-use std::os::raw::c_void;
-use std::mem::{forget};
-use std::ptr::null_mut;
 use std::ops::Deref;
+use std::os::raw::c_void;
+use std::path::PathBuf;
+use std::ptr::null_mut;
 use std::sync::mpsc::TrySendError::Full;
-use vst3_interfaces::*;
+use structopt::StructOpt;
 use vst3_interfaces::vst::SymbolicSampleSizes::{kSample32, kSample64};
+use vst3_interfaces::*;
+use winit::dpi::LogicalSize;
+use winit::{Event, EventsLoop, WindowBuilder, WindowEvent};
+use x11::xlib::XInternAtom;
 
 mod host_impl;
 use host_impl::*;
@@ -34,38 +34,37 @@ use winit::os::unix::WindowExt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "cli-host")]
 struct Opt {
-    #[structopt(short= "p", long = "path")]
+    #[structopt(short = "p", long = "path")]
     #[structopt(parse(from_os_str))]
-    path : PathBuf
+    path: PathBuf,
 }
 
-fn open (controller : Editor) {
+fn open(controller: Editor) {
     let view = controller.create_view();
     let size = view.get_size().unwrap();
 
     let mut events_loop = EventsLoop::new();
     let window = WindowBuilder::new()
-        .with_dimensions(LogicalSize::new(size.right as f64,size.bottom as f64))
+        .with_dimensions(LogicalSize::new(size.right as f64, size.bottom as f64))
         .with_title("VST3 Hosted in Rust!")
-        .build(&events_loop).unwrap();
+        .build(&events_loop)
+        .unwrap();
 
-    if let Some (display) = window.get_xlib_display() {
-        let atom = unsafe {
-            XInternAtom (
-                display as *mut _,
-                transmute("_XEMBED_INFO\0".as_ptr()),
-                1)
-        };
-        if atom == 0 { panic!("XEMBED_INFO does not exist"); }
+    if let Some(display) = window.get_xlib_display() {
+        let atom =
+            unsafe { XInternAtom(display as *mut _, transmute("_XEMBED_INFO\0".as_ptr()), 1) };
+        if atom == 0 {
+            panic!("XEMBED_INFO does not exist");
+        }
         let frame = HostPlugFrame::new();
 
         view.set_frame(frame.as_raw());
-        view.attached (display)
+        view.attached(display)
             .expect("failed to attach to platform handle");
 
         loop {
-            events_loop.poll_events(|event| {
-                match event { _ => () }
+            events_loop.poll_events(|event| match event {
+                _ => (),
             });
         }
     }
@@ -77,22 +76,14 @@ fn main() {
 
     lib_path.push("Contents");
     lib_path.push("x86_64-linux"); //todo: cross platform version
-    lib_path.push(
-        opt.path.as_path()
-            .file_stem()
-            .unwrap()
-            .to_str()
-            .unwrap());
+    lib_path.push(opt.path.as_path().file_stem().unwrap().to_str().unwrap());
     lib_path.set_extension("so");
 
-    let lib = ll::Library::new(lib_path
-        .as_path())
-        .expect("no such library");
+    let lib = ll::Library::new(lib_path.as_path()).expect("no such library");
 
     let mut instances: Vec<(VstPtr<FUnknown>, PClassInfo)> = Vec::new();
     let GetPluginFactory: ll::Symbol<GetFactoryProc> = unsafe {
-        lib
-            .get(b"GetPluginFactory")
+        lib.get(b"GetPluginFactory")
             .expect("No Plugin Factory Defined")
     };
 
@@ -108,7 +99,8 @@ fn main() {
             (*factory).createInstance(
                 info.cid.as_ptr(),
                 FUnknown::iid().as_ptr(),
-                &mut ptr as *mut _);
+                &mut ptr as *mut _,
+            );
             instances.push((VstPtr::from_raw(ptr as *mut _), info));
         }
     }
@@ -126,15 +118,21 @@ fn main() {
             if let Ok(_) = proc.set_stereo() {
                 println!("\tstereo supported");
             }
-            println!("\ttail samples: {}\n\tlatency samples: {}", proc.tail_samples(), proc.latency_samples());
+            println!(
+                "\ttail samples: {}\n\tlatency samples: {}",
+                proc.tail_samples(),
+                proc.latency_samples()
+            );
 
-            if proc.supports32() { println!("\t32 bit audio supported"); }
-            if proc.supports64() { println!("\t64 bit audio supported"); }
-            if let Ok(_) = proc.setup_processing(
-                ProcessMode::Offline,
-                SampleSize::F32,
-                512,
-                48000.0) {
+            if proc.supports32() {
+                println!("\t32 bit audio supported");
+            }
+            if proc.supports64() {
+                println!("\t64 bit audio supported");
+            }
+            if let Ok(_) =
+                proc.setup_processing(ProcessMode::Offline, SampleSize::F32, 512, 48000.0)
+            {
                 println!("\tready for offline processing");
             }
         }
